e07e0611d497cf41d5e3a591c0102e259ef8f0ea
duanlingbosjtu <duanlingbosjtu@sjtu.edu.cn>
2021-10-05 09:59:46 +0800

    Save

--- VG151/Homework/h2/ex4.m
+++ VG151/Homework/h2/ex4.m
@@ -1,3 +1,22 @@
+function ex4()
+	s=input('','s');
+	f=str2func(s);
+	x=input('');
+	y=input('');
+	dfs(f,x,y,2);
+end
+
+function dfs(f,x,y,i)
+	if (abs(x(i)-x(i-1))<power(10,-y))
+		format longg;
+		disp(roundn(x(i),-y));
+		return;
+	end
+	x(i+1)=(x(i-1)*f(x(i))-x(i)*f(x(i-1)))/(f(x(i))-f(x(i-1)));
+	dfs(f,x,y,i+1);
+end
+
+function ex41()
 s=input('','s');
 f=str2func(s);
 x=input('');
@@ -9,3 +28,4 @@ while (abs(x(i)-x(i-1))>=power(10,-y))
 end
 format longg;
 disp(roundn(x(i),-y));
+end
