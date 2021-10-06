4bdbf2b1debc5098ae282775cddfd653aae88f63
paul120090105 <paul120090105@yahoo.com>
2021-10-06 23:32:48 +0800

    Save

--- VG151/Project/P1/Project1_1/CreateNewPlate.m
+++ VG151/Project/P1/Project1_1/CreateNewPlate.m
@@ -9,8 +9,27 @@ function Plate=CreateNewPlate()
         cnt=cnt+1;
         CharPool(cnt)=char(i);
     end
+    disp(cnt);
 	% Shuffle this pool
     CharPool=CharPool(randperm(cnt));
+    Use=zeros(cnt); PlatePool=[]; Plate=[];
+    PlatePool=RecursionForPlate(1,PlatePool,Plate,CharPool,Use);
 	% Create a new plate by taking the first five char
-    Plate=strcat(char(floor(rand()*26)+65),' ',CharPool(1:5));
+    Len=length(PlatePool);
+    Plate=strcat(char(floor(rand()*26)+65),' ',PlatePool(floor(rand()*(Len-1))+1));
+end
+
+function PlatePool=RecursionForPlate(Step,PlatePool,Plate,Pool,Use)
+    if (Step==6)
+        PlatePool=[PlatePool string(char(Plate))];
+        return;
+    end
+    if (length(PlatePool)>500) return; end
+    for i=1:36
+        if (Use(i)) continue; end
+        Use(i)=1;
+        Plate(Step)=char(Pool(i));
+        PlatePool=RecursionForPlate(Step+1,PlatePool,Plate,Pool,Use);
+        Use(i)=0;
+    end
 end
